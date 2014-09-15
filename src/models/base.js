0d0639e9ae6af3c85e5f85703aab012551e96daf
Mark Terry <mark.terry@holidayextras.com>
2014-09-15 12:37:39 +0100

    wip 2

---
+++ src/models/base.js
@@ -0,0 +1,20 @@
+var logger = require('lib/logger');
+var DbModels = require('lib/models/db/');
+
+var ModelBase = function(dbModel){
+  this.__db = dbModel;
+};
+
+ModelBase.prototype.save = function(cb){
+ this.__db.save(function(err){
+   if(err) return logger.error({message: err});
+   if(cb) cb();
+ });
+};
+
+//data that is contained in this instance
+TripBooking.prototype.data = function(){
+  return this.__db;
+};
+
+module.exports = ModelBase;
---
+++ src/models/db/index.js
@@ -0,0 +1,6 @@
+var DbModels = {};
+
+DbModule.Trip = require('models/trip');
+DbModule.TripBooking = require('models/trip_booking');
+
+module.exports = DbModels;
